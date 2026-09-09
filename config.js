6070d52212728bad4c239ebc0b5713c90ab2606c
FabioBarontini <94986269+FabioBarontini@users.noreply.github.com>
2026-09-09 17:08:53 +0200

    Update SUBMIT_ENDPOINT in config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // Students do not need to edit this file.
 // For central collection, paste your Google Apps Script Web App URL here.
 window.BLACKOUT_CONFIG = {
-  SUBMIT_ENDPOINT: "https://script.google.com/u/0/home/projects/1eR13Y3XKGczQDOLJE-0ibHFJyT_qOhJGBlXEwnuZT3NqUR1mdsGzDvT8/exec",
+  SUBMIT_ENDPOINT: "https://script.google.com/macros/s/AKfycbwiPKE6hLJJDqPjVHrzPmpsFbwVFJyF972597GOA_hMkaU3KJ9DV9TuamRGVD6L1Wdi/exec",
   CASE_ID: "HX-047"
 };
