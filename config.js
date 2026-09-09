4b1f368b52969575dfdc0a54a5e4a1841168b09f
FabioBarontini <94986269+FabioBarontini@users.noreply.github.com>
2026-09-09 17:02:34 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // Students do not need to edit this file.
 // For central collection, paste your Google Apps Script Web App URL here.
 window.BLACKOUT_CONFIG = {
-  SUBMIT_ENDPOINT: "",
+  SUBMIT_ENDPOINT: "https://script.google.com/macros/s/AKfycbwiPKE6hLJJDqPjVHrzPmpsFbwVFJyF972597GOA_hMkaU3KJ9DV9TuamRGVD6L1Wdi/exec",
   CASE_ID: "HX-047"
 };
